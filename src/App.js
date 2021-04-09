a8dd710b5babae226e6bea0b39a97b8c9326a26a
Qunli <qunli02@gmail.com>
2021-04-09 15:55:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Nav from "./nav.js";
+import Region from "./region.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <div>
+        <Region />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/region.js
@@ -0,0 +1,14 @@
+import "./region.css";
+
+function Region() {
+  return (
+    <div className="region">
+      <ul className="region-list">
+        <il>United States</il>
+        <il>Canda</il>
+      </ul>
+    </div>
+  );
+}
+
+export default Region;

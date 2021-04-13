1e4978c06580603b615bdd10de54042b37fb300b
Qunli <qunli02@gmail.com>
2021-04-12 22:53:30 -0400

    layout

--- src/region.js
+++ src/region.js
@@ -2,11 +2,15 @@ import "./region.css";
 
 function Region() {
   return (
-    <div className="region">
+    <div className="region flex">
       <ul className="region-list">
         <il>United States</il>
         <il>Canda</il>
       </ul>
+      <div>
+        <div>sdsd</div>
+        <div>sdjksn</div>
+      </div>
     </div>
   );
 }
